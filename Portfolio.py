e4e609ff911eb28f0031f0a8ae95b2598a446b18
Biruk <biruknicholas@gmail.com>
2025-06-06 14:50:30 +0300

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('protfolio.html')
+    return render_template('portfolio.html')
 
 @app.route('/about')
 def about():
